db0b27a3277ce8bdf1d41ce0673f523e8ad0f752
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2014-11-19 18:31:47 -0500

    Add bootstrap styling to users list.

--- application/views/user/list.php
+++ application/views/user/list.php
@@ -1,30 +1,35 @@
-<h2>User Table</h2>
+<section class="closed-container">
+  
+  <h2 class="container-header">User Table</h2>
 
-<table>
-  <tr>
-    <th>id</th>
-    <th>login</th>
-    <th>first name</th>
-    <th>last name</th>
-    <th>email</th>
-  </tr>
-<?php
-  foreach ($users as $user) {
-    echo "<tr>";
-    echo "<td>" . $user->id . "</td>";
-    echo "<td>" . $user->login . "</td>";
-    echo "<td>" . $user->first . "</td>";
-    echo "<td>" . $user->last . "</td>";
-    echo "<td>" . $user->email . "</td>";
+  <table>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>login</th>
+        <th>first name</th>
+        <th>last name</th>
+        <th>email</th>
+      </tr>
+    </thead>
+  <?php
+    foreach ($users as $user) {
+      echo "<tr>";
+      echo "<td>" . $user->id . "</td>";
+      echo "<td>" . $user->login . "</td>";
+      echo "<td>" . $user->first . "</td>";
+      echo "<td>" . $user->last . "</td>";
+      echo "<td>" . $user->email . "</td>";
 
-    echo "<td>" . anchor("user/show/$user->id", 'Show') . "</td>";
-    echo "<td>" . anchor("user/edit/$user->id", 'Edit') . "</td>";
-    echo "<td>" . anchor("user/delete/$user->id", 'Delete',
-                          "onClick='return confirm(".
-                            '"Do you really want to delete this record?"'.
-                          ");'"
-                        ) . "</td>";
-    echo "</tr>";
-  }
-?>
-<table>
+      echo "<td>" . anchor("user/show/$user->id", 'Show') . "</td>";
+      echo "<td>" . anchor("user/edit/$user->id", 'Edit') . "</td>";
+      echo "<td>" . anchor("user/delete/$user->id", 'Delete',
+                            "onClick='return confirm(".
+                              '"Do you really want to delete this record?"'.
+                            ");'"
+                          ) . "</td>";
+      echo "</tr>";
+    }
+  ?>
+  </table>
+</section><!-- ./closed-container -->
