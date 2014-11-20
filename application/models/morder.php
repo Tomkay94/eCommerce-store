4c31aa318d0b0570bcb308d6fe9ca49e9f0d9a0c
Sunny Li <LDYsunny@hotmail.com>
2014-11-20 15:57:32 -0500

    more Order related stuff

--- application/models/morder.php
+++ application/models/morder.php
@@ -24,15 +24,28 @@ class MOrder extends CI_Model {
     return $query->row(0, 'Order');
   }
 
-  function insert($order) {
-    // wrap a transaction when calling this function
-    return $this->db->insert("orders", array('customer_id' => $order->customer_id,
-                                            'order_date' => $order->order_date,
-                                            'order_time' => $order->order_time,
-                                            'total' => $order->total,
-                                            'creditcard_number' => $order->creditcard_number,
-                                            'creditcard_month' => $order->creditcard_month,
-                                            'creditcard_year' => $order->creditcard_year));
+  // create order objects and the items together in an trasaction
+  // $order is an Order object, $order_items is an shopping cart array..
+  function insert($order, $order_items) {
+    $this->db->trans_start();
+    // create order
+    $this->db->insert("orders", array('customer_id' => $order->customer_id,
+                                      'order_date' => $order->order_date,
+                                      'order_time' => $order->order_time,
+                                      'total' => $order->total,
+                                      'creditcard_number' => $order->creditcard_number,
+                                      'creditcard_month' => $order->creditcard_month,
+                                      'creditcard_year' => $order->creditcard_year));
+    $order_id = $this->db->insert_id();
+    // create order items
+    foreach ($order_items as $order_item) {
+      $this->db->insert("order_items", array('order_id' => $order_id,
+                                             'product_id' => $order_item['id'],
+                                             'quantity' => $order_item['qty']));
+    }
+    $this->db->trans_complete();
+
+    return $this->db->trans_status();
   }
 
   function delete($id) {
--- application/models/order_item.php
+++ application/models/order_item.php
@@ -1,7 +1,14 @@
 <?php
-class Order_Item {
+class Order_Item extends CI_Model {
   public $id;
   public $order_id;
   public $product_id;
   public $quantity;
+
+  function find($id) {
+    $query = $this->db->get_where('order_items',array('id' => $id));
+    return $query->row(0, 'Order_Item');
+  }
+
+  // delete will automatically handled by database constraints
 }
